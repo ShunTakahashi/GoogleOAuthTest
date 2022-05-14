ccfffc97f3c920124dbc4e4381cd68a8d2d04f53
ShunTakahashi <tshun.1996@icloud.com>
2022-05-14 16:34:50 +0900

    Google認証追加

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,11 @@ ActiveRecord::Schema.define(version: 2022_05_14_072819) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "users"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "provider"
     t.string "uid"
     t.string "token"
