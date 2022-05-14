4ab81f216f4909e031fe4c475dffa7b6f93b3302
ShunTakahashi <tshun.1996@icloud.com>
2022-05-14 22:44:21 +0900

    post method追加

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class OmniauthCallbacksController < ApplicationController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google
     @user = User.find_for_google(request.env['omniauth.auth'])
 
@@ -9,5 +9,9 @@ class OmniauthCallbacksController < ApplicationController
       session['devise.google_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
+  end
+
+  def after_sign_in_path_for(resource)
+    home_index_path
   end
 end
